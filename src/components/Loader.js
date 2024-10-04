9f71f4a8353af84dd3e44bdfe81637752f73b8b9
Hridayesh12 <hridayeshpadalkar7@gmail.com>
2024-10-04 11:47:01 +0530

    Done

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Loader = () => {
     return (
         <div className='flex items-center justify-center min-h-screen bg-base-600'>
-            <img src="http://3.111.16.234:3000/assets/loader.gif" alt="Loading..." />
+            <img src="http://192.168.1.131:3000/assets/loader.gif" alt="Loading..." />
         </div>
     );
 };
