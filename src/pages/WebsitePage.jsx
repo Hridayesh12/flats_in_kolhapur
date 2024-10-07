96a5868bfe3db303610e430b0f53278930c108e1
Hridayesh12 <hridayeshpadalkar7@gmail.com>
2024-10-07 22:33:16 +0530

    Main

--- src/pages/WebsitePage.jsx
+++ src/pages/WebsitePage.jsx
@@ -114,7 +114,7 @@ function WebsitePage() {
             />
     <meta property="og:title" content={`${project?.title}`} />
     <meta property="og:description" content={`${project?.description}`} />
-    <meta property="og:image" content={`${`https://flats-in-kop-backend.onrender.com:5000/v1${project?.displayImage}`}`} />
+    <meta property="og:image" content={`${`https://flats-in-kop-backend.vercel.app/v1${project?.displayImage}`}`} />
     <meta property="og:url" content={`${`https://flats-in-kolhapur.vercel.app/${domain}`}`} />
     <meta property="og:type" content="website" />
           </Helmet>
